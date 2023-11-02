b15922d4cf9a695f31fe7c03e7b665501fe77d7d
Prem Singh <22cspre036@ldce.ac.in>
2023-11-02 10:35:32 +0530

    code block ide used - cg smart village project
    code block ide used - cg smart village project

---
+++ main.cpp
@@ -0,0 +1,442 @@
+#include<windows.h>
+#include<GL\glut.h>
+#include <GL/glu.h>
+#include<math.h>
+#include <stdlib.h>
+#include<stdio.h>
+#define PI 3.1416
+
+GLint i, j, k;
+GLfloat sun_spin=0, sun_x=0, sun_y=0;
+GLfloat ax=0,bx=0,cx=0,dx=0,str=500.0,mn=500.0;
+GLfloat sr=0.0,sg=0.749,sb=1.0;
+GLfloat spin = 0.0;
+
+void init(void)
+{
+	glClearColor(.40, .110, 1.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0.0, 1000.0, 0.0, 500.0);
+}
+
+
+///============================================================================================================///
+
+///=================///
+///*** All_Model ***///
+///=================///
+
+
+
+///*** Circle_Model***///
+void circle(GLdouble rad)
+{
+    GLint points = 50;
+    GLdouble delTheta = (2.0 * PI) / (GLdouble)points;
+    GLdouble theta = 0.0;
+
+    glBegin(GL_POLYGON);
+    {
+        for( i = 0; i <=50; i++, theta += delTheta )
+        {
+            glVertex2f(rad * cos(theta),rad * sin(theta));
+        }
+    }
+    glEnd();
+}
+/// *** Sun_Model **///
+void Sun_Model(){
+    glPushMatrix();
+    glTranslatef(500,0,0);
+    circle(30);
+    glPopMatrix();
+
+}
+void Moving_Sun_Model(){
+    glPushMatrix();
+    glRotatef(-sun_spin, 0,0,-.009);
+    Sun_Model();
+    glPopMatrix();
+
+}
+///*** Cloud_Model***///
+void cloud_model_Two(){
+    glColor3f(1.25, 0.924, 0.930);
+
+    ///Left_Part
+    glPushMatrix();
+    glTranslatef(305,205,0);
+    circle(10);
+    glPopMatrix();
+
+    ///Top
+
+    glPushMatrix();
+    glTranslatef(320,210,0);
+    circle(15);
+    glPopMatrix();
+
+    ///Right_Part
+    glPushMatrix();
+    glTranslatef(334,207,0);
+    circle(10);
+    glPopMatrix();
+
+    ///Bottom_Part
+    glPushMatrix();
+    glTranslatef(320,207,0);
+    circle(10);
+    glPopMatrix();
+
+
+
+}
+///*** Hill_Model***///
+void hill_big(){
+
+    ///Hill
+	glBegin(GL_POLYGON);
+    glColor3f(0.38, 0.41, 0.36);
+	glVertex2i(70, 70);
+	glVertex2i(200, 225);
+	glVertex2i(330, 70);
+
+	glEnd();
+
+	///Hill_Snow
+    glBegin(GL_POLYGON);
+    glColor3f(1.25, 0.924, 0.930);
+
+	glVertex2i(200, 225);
+	glVertex2i(230, 190);
+	glVertex2i(220, 180);
+	glVertex2i(200, 190);
+	glVertex2i(190, 180);
+	glVertex2i(170, 190);
+
+	glEnd();
+
+}
+///*** Field_Model ***///
+void field(){
+    ///Field
+	glBegin(GL_POLYGON);
+    glColor3f(0.533, 1.293, 0.0);
+	glVertex2i(0, 50);
+	glVertex2i(0, 70);
+	glVertex2i(1000, 70);
+	glVertex2i(1000, 50);
+
+	glEnd();
+
+
+    ///Field_Shadow
+	glBegin(GL_POLYGON);
+	glColor3f(0.1, 1.293, 0.0);
+	glVertex2i(0, 0);
+	glVertex2i(0, 50);
+	glVertex2i(1000, 50);
+	glVertex2i(1000, 0);
+
+	glEnd();
+
+
+}
+///*** Tree_Model ***///
+void Tree_Model_One(){
+
+
+    glPushMatrix();
+    glTranslatef(110,110,0);
+    circle(15);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(110,100,0);
+    circle(15);
+    glPopMatrix();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.38, 0.21, 0.26);
+	glVertex2f(109, 70);
+	glVertex2f(109, 90);
+	glVertex2f(111, 90);
+	glVertex2f(111, 70);
+
+    glEnd();
+
+}
+
+
+/// *** Windmill_Stand_Model ***///
+void Windmill_Stand_Model(){
+
+    glColor3f(0.38, 0.41, 0.36);
+    glBegin(GL_POLYGON);
+    glVertex2i(375, 100);
+    glVertex2i(380, 240);
+    glVertex2i(384, 240);
+    glVertex2i(390, 100);
+    glEnd();
+}
+
+///*** Windmill_Blades_Model ***///
+
+void Windmill_Blade(){
+
+    ///Blade_One
+    glPushMatrix();
+    glRotatef(spin,0,0,90);
+    glBegin(GL_POLYGON);
+    glVertex2i(-5, 0);
+    glVertex2i(-85, -36);
+    glVertex2i(-83, -37);
+    glVertex2i(-3, -8);
+    glEnd();
+    glPopMatrix();
+
+    ///Blade_Two
+    glPushMatrix();
+    glRotatef(spin,0,0,90);
+    glBegin(GL_POLYGON);
+    glVertex2i(0, 5);
+    glVertex2i(45, 70);
+    glVertex2i(50, 73);
+    glVertex2i(5, 0);
+    glEnd();
+    glPopMatrix();
+
+    ///Blade_Three
+    glPushMatrix();
+    glRotatef(spin,0,0,90);
+    glBegin(GL_POLYGON);
+    glVertex2i(68, -78);
+    glVertex2i(0,0);
+    glVertex2i(5, 5);
+    glVertex2i(70, -77);
+    glEnd();
+    glPopMatrix();
+
+}
+///*** Windmill_Final_Model ***///
+void Windmill(){
+
+
+
+        ///Windmill_Stand
+    glColor3f(0.38, 0.41, 0.36);
+    glPushMatrix();
+    Windmill_Stand_Model();
+    glPopMatrix();
+
+    ///Windmill_Motor
+    glColor3f(0.11, 0.23, 0.36);
+    glPushMatrix();
+    glTranslatef(380,250,0);
+    circle(10);
+    glPopMatrix();
+
+    ///Windmill_Rotary_Blades
+    glColor3f(0.11, 0.23, 0.36);
+    glPushMatrix();
+    glTranslatef(380,251,0);
+    Windmill_Blade();
+    glPopMatrix();
+
+
+
+
+}
+
+
+///Model_End
+///=======================================================================================================///
+
+
+///=================///
+///***   Object  ***///
+///=================///
+
+///*** Sun ***///
+void Sun(){
+    glColor3f(1, 1, 0);
+    glPushMatrix();
+    Moving_Sun_Model();
+    glPopMatrix();
+}
+
+
+
+
+///*** Cloud_Four_Model_Two ***///
+
+void cloud_four(){
+    glPushMatrix();
+    glTranslatef(dx+300,125,0);
+    cloud_model_Two();
+    glPopMatrix();
+
+}
+
+
+
+
+///*** Hill_big_One ***///
+void Hill_Big_One(){
+    glPushMatrix();
+    glTranslatef(0,0,0);
+    hill_big();
+    glPopMatrix();
+}
+///*** Hill_big_Two ***///
+void Hill_Big_Two(){
+    glPushMatrix();
+    glTranslatef(550,-20,0);
+    hill_big();
+    glPopMatrix();
+}
+
+
+
+
+///*** Tree_2 ***///
+void Tree_Two(){
+    glColor3f(0.533, 1.293, 0.0);
+    glPushMatrix();
+    glTranslatef(540,0,0);
+    Tree_Model_One();
+    glPopMatrix();
+}
+
+///*** Tree_3 ***///
+void Tree_Three(){
+    glColor3f(0.533, 1.293, 0.0);
+    glPushMatrix();
+    glTranslatef(750,0,0);
+    Tree_Model_One();
+    glPopMatrix();
+}
+
+
+/// *** Windmill ***///
+
+
+void Windmill_Two(){
+    glColor3f(0.11, 0.23, 0.36);
+    glPushMatrix();
+    glTranslatef(508,-70,0);
+    Windmill();
+    glPopMatrix();
+
+}
+void Windmill_Three(){
+    glColor3f(0.11, 0.23, 0.36);
+    glPushMatrix();
+    glTranslatef(108,-90,0);
+    Windmill();
+    glPopMatrix();
+
+}
+///Object_End
+///=========================================================================================================///
+
+
+///========================///
+///*** Display Function ***///
+///========================///
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0, 0.0, 1.0);
+
+    ///*** Object_Layer ***///
+    Sun();
+
+    Windmill_Three();
+    Hill_Big_One();
+    Hill_Big_Two();
+
+    cloud_four();
+    Windmill_Two();
+
+     Tree_Two();
+     Tree_Three();
+     field();
+
+	glFlush();
+}
+///========================///
+///*** Speed & Movement ***///
+///========================///
+///*** Sun_Move ***///
+void sun_move(){
+
+    sun_spin = sun_spin + 0.0008;
+
+}
+void move_right(){
+
+    sun_move();
+
+    spin = spin +.1;
+    ax = ax + .05;
+    bx = bx + .08;
+    cx = cx + .10;
+    dx = dx + .15;
+
+    if(cx>1000){
+        cx = -300;
+    }
+    if(bx>1000){
+            bx= -400;
+
+    }
+    if(cx>1000){
+            cx= -400;
+
+    }
+    if(dx>1000){
+            dx= -500;
+
+    }
+
+
+    glutPostRedisplay();
+}
+
+
+
+void mouse(int key, int state, int x, int y){
+    switch (key)
+    {
+    case GLUT_LEFT_BUTTON:
+        if (state == GLUT_DOWN)
+        {
+            glutIdleFunc(move_right);
+        }
+        break;
+    case GLUT_MIDDLE_BUTTON:
+    case GLUT_RIGHT_BUTTON:
+        if (state == GLUT_DOWN)
+        {
+            glutIdleFunc(NULL);
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(50, 50);
+	glutInitWindowSize(1900, 1900);
+	glutCreateWindow("Smart Village");
+	init();
+	glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+	glutMainLoop();
+}
